3688a1263d7f223d5ea08a756afe31c4d1e2b0c4
Chris Pickford <chris@luminousuk.com>
2017-11-28 14:57:31 +0000

    Add files via upload

---
+++ WcfSecurityTest.Service/IService1.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace WcfSecurityTest.Service
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        [DataMember]
+        public bool BoolValue { get; set; } = true;
+
+        [DataMember]
+        public string StringValue { get; set; } = "Hello ";
+    }
+}
---
+++ WcfSecurityTest.Service/Service1.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WcfSecurityTest.Service
+{
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return $"You entered: {value}";
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+                throw new ArgumentNullException(nameof(composite));
+            if (composite.BoolValue)
+                composite.StringValue += "Suffix";
+            return composite;
+        }
+    }
+}
